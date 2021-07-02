e10cda6039cbcec536c9ec0a1b6c2705ff3be824
Thomas Klebel <t.klebel@hotmail.com>
2021-07-02 14:47:47 +0200

    create var overview

---
+++ scripts/01-var-overview.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+label_base <- read_csv("data/label_base.csv")
+
+
+var_labels <- tibble(var_names = names(label_base))
+
+var_labels %>% 
+  separate(var_names, into = c("short_code", "question_text"),
+           sep = "\\.\\s") %>% 
+  mutate(question_specification = str_extract(question_text, 
+                                              "(?<=\\[).*?(?=\\])")) %>% 
+  write_csv("data/processed/question_codes.csv")
