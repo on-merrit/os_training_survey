1f0881f4d799f5e9af8f49abe7194070bcd503f8
Thomas Klebel <t.klebel@hotmail.com>
2021-07-02 14:59:17 +0200

    fix var overview

--- scripts/01-var-overview.R
+++ scripts/01-var-overview.R
@@ -7,7 +7,7 @@ var_labels <- tibble(var_names = names(label_base))
 
 var_labels %>% 
   separate(var_names, into = c("short_code", "question_text"),
-           sep = "\\.\\s") %>% 
+           sep = "\\.\\s", extra = "merge") %>% 
   mutate(question_specification = str_extract(question_text, 
                                               "(?<=\\[).*?(?=\\])")) %>% 
   write_csv("data/processed/question_codes.csv")
