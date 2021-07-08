abcd8ba8f42fdda1141b8d82427ae97f9c2ce8ce
Thomas Klebel <t.klebel@hotmail.com>
2021-07-08 14:33:35 +0200

    move targetes code to _targets.R

---
+++ _targets.R
@@ -0,0 +1,35 @@
+library(targets)
+source("R/functions.R")
+source("R/helpers.R")
+options(tidyverse.quiet = TRUE)
+tar_option_set(packages = c("scales", "stringr", "tidyverse"))
+
+list(
+  tar_target(
+    raw_data_file,
+    "data/processed/survey_anonymised.csv",
+    format = "file"
+  ),
+  tar_target(
+    raw_data,
+    read_csv(raw_data_file)
+  ),
+  tar_target(
+    countries_file,
+    "data/processed/countries_merged.csv",
+    format = "file"
+  ),
+  tar_target(
+    countries,
+    read_csv(countries_file)
+  ),
+  tar_target(
+    only_consenters,
+    raw_data %>%
+      remove_non_consenters()
+  ),
+  tar_target(
+    with_countries,
+    add_country(only_consenters, countries)
+  )
+)
