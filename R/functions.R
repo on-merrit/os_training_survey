64ea5a23a77ee42d22ef971e71528b83d2b0195c
anjarainer <rainer.anja@live.at>
2021-07-19 15:59:00 +0200

    add ggplot_lollipop to plot_data

--- R/functions.R
+++ R/functions.R
@@ -24,26 +24,32 @@ add_country <- function(raw_data, merged_countries) {
     left_join(countries_distinct, by = "E1") 
 }
 
+nudge_y <- .04
 
 plot_bar <- function(df,  var, title = NULL, reorder = TRUE) {
   if (reorder) {
     plot_data <- df %>%
       count({{var}}) %>%
       mutate(prop = n/sum(n),
+             label = glue::glue("{n} ({scales:percent(prop, accuarcy = .1)})"),
              xvar = fct_reorder({{var}}, n, .fun = "max"))
   } else {
     plot_data <- df %>%
       count({{var}}) %>%
       mutate(prop = n/sum(n),
+             label = glue::glue("{n} ({scales:percent(prop, accuarcy = .1)})"),
              xvar = {{var}})
   }
   
   
   plot_data %>%
     ggplot(aes(xvar, prop)) +
+    geom_lollipop() +
+    coord_flip(clip = "off") +
     geom_col(width = .7) +
-    scale_y_continuous(labels = scales::percent) + 
-    coord_flip() +
+    geom_text(aes(label = label, nudge_y = nudge_y)) +
+    scale_y_continuous(labels = function(x) scales::percent(x, accurarcy = 1,
+                        breaks = c(0, .1, .2, .3))) + 
     labs(x = NULL, title = title)
 }
 
