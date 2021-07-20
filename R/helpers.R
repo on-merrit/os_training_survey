f09e4ec4c03b85978baa70a8ea5236bd55d98543
Thomas Klebel <t.klebel@hotmail.com>
2021-07-20 16:30:12 +0200

    move plotting function into helpers

--- R/helpers.R
+++ R/helpers.R
@@ -31,3 +31,32 @@ recode_successful <- function(df1, df2) {
       summarise(across(.fns = ~sum(is.na(.x))))
   )
 }
+
+
+plot_bar <- function(df, var, title = NULL, reorder = TRUE, nudge_y = .04,
+                     y_lab = NULL) {
+  if (reorder) {
+    plot_data <- df %>%
+      count({{var}}) %>%
+      mutate(prop = n/sum(n),
+             label = glue::glue("{n} ({scales::percent(prop, accuarcy = .1)})"),
+             xvar = fct_reorder({{var}}, n, .fun = "max"))
+  } else {
+    plot_data <- df %>%
+      count({{var}}) %>%
+      mutate(prop = n/sum(n),
+             label = glue::glue("{n} ({scales::percent(prop, accuarcy = .1)})"),
+             xvar = {{var}})
+  }
+  
+  
+  plot_data %>%
+    filter(!is.na({{var}})) %>%
+    ggplot(aes(xvar, prop)) +
+    geom_lollipop() +
+    coord_flip(clip = "off") +
+    geom_text(aes(label = label), nudge_y = nudge_y) +
+    scale_y_continuous(labels = function(x) scales::percent(x, accurarcy = 1)) + 
+    labs(x = NULL, y = y_lab) +
+    hrbrthemes::theme_ipsum(base_family = "Hind", grid = "")
+}
