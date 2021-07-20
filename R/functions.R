d1e895c5b5b6b87b40dc15c875bc5b9dfac2b7e4
Thomas Klebel <t.klebel@hotmail.com>
2021-07-20 10:00:27 +0200

    further adapt univariate plot

--- R/functions.R
+++ R/functions.R
@@ -24,20 +24,20 @@ add_country <- function(raw_data, merged_countries) {
     left_join(countries_distinct, by = "E1") 
 }
 
-nudge_y <- .04
 
-plot_bar <- function(df,  var, title = NULL, reorder = TRUE) {
+plot_bar <- function(df, var, title = NULL, reorder = TRUE, nudge_y = .04,
+                     y_lab = NULL) {
   if (reorder) {
     plot_data <- df %>%
       count({{var}}) %>%
       mutate(prop = n/sum(n),
-             label = glue::glue("{n} ({scales:percent(prop, accuarcy = .1)})"),
+             label = glue::glue("{n} ({scales::percent(prop, accuarcy = .1)})"),
              xvar = fct_reorder({{var}}, n, .fun = "max"))
   } else {
     plot_data <- df %>%
       count({{var}}) %>%
       mutate(prop = n/sum(n),
-             label = glue::glue("{n} ({scales:percent(prop, accuarcy = .1)})"),
+             label = glue::glue("{n} ({scales::percent(prop, accuarcy = .1)})"),
              xvar = {{var}})
   }
   
@@ -46,10 +46,9 @@ plot_bar <- function(df,  var, title = NULL, reorder = TRUE) {
     ggplot(aes(xvar, prop)) +
     geom_lollipop() +
     coord_flip(clip = "off") +
-    geom_col(width = .7) +
-    geom_text(aes(label = label, nudge_y = nudge_y)) +
-    scale_y_continuous(labels = function(x) scales::percent(x, accurarcy = 1,
-                        breaks = c(0, .1, .2, .3))) + 
-    labs(x = NULL, title = title)
+    geom_text(aes(label = label), nudge_y = nudge_y) +
+    scale_y_continuous(labels = function(x) scales::percent(x, accurarcy = 1)) + 
+    labs(x = NULL, y = y_lab) +
+    hrbrthemes::theme_ipsum(base_family = "Hind", grid = "")
 }
 
